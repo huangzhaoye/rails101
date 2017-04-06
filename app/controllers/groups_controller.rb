57873bb04f962af694647931aab7dcdd3dc14e94
sisanq163 <sisanq168@gmail.com>
2017-04-06 20:05:30 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :authenticate_user! , only: [:new, :create]
-  before_action :authenticate_user! , only: [:new]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
       @groups = Group.all
     end
@@ -11,10 +11,7 @@ class GroupsController < ApplicationController
      end
 
       def edit
-       @group = Group.find(params[:id])
-       if current_user != @group.user
-          redirect_to root_path, alert: "You have no permission."
-           end
+            end
       end
 
      def new
@@ -33,10 +30,6 @@ class GroupsController < ApplicationController
        end
 
        def update
-         @group = Group.find(params[:id])
-         if current_user != @group.user
-             redirect_to root_path, alert: "You have no permission."
-           end
 
          if @group.update(group_params)
            redirect_to groups_path, notice: "Update Success"
@@ -46,11 +39,6 @@ class GroupsController < ApplicationController
        end
 
           def destroy
-            @group = Group.find(params[:id])
-
-            if current_user != @group.user
-               redirect_to root_path, alert: "You have no permission."
-             end
 
             @group.destroy
             flash[:alert] = "Group deleted"
@@ -59,7 +47,14 @@ class GroupsController < ApplicationController
 
       private
 
+      def find_group_and_check_permission
+          @group = Group.find(params[:id])
+
+          if current_user != @group.user
+            redirect_to root_path, alert: "You have no permission."
+          end
+        end
+
       def group_params
         params.require(:group).permit(:title, :description)
       end
-end
